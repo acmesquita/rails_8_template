1433aba2a72d8d567578d811affdcf7e4a860044
Catharina Mesquita <catha.ana.1994@gmail.com>
2025-05-22 18:07:53 -0300

    change to using turbo frame

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @users = @users.where("email LIKE ? or name LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%") if params[:q].present?
-    @users = @users.where(status: params[:status]) if params[:status].present?
+
+    if params[:query].present?
+      @users = @users.where("name LIKE ? OR email LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    end
+
+    if params[:status].present?
+      @users = @users.where(status: params[:status])
+    end
+
     @users = @users.order(created_at: :desc)
   end
 end
